d9bc10f10d2e061e9e35da850a2c9c8ae165f92f
Krygore <webdev.krygore@gmail.com>
2022-10-24 21:42:59 -0700

    completed guided project

--- src/components/App.js
+++ src/components/App.js
@@ -14,21 +14,35 @@ const initialFormValues = {
 
 export default function App() {
   const [friends, setFriends] = useState([]) // careful what you initialize your state to
-
+  const [formError, setFormError] =useState('')
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
-  const formValues = {} // fix this using the state hook
+  const [formValues, setFormValues] = useState(initialFormValues); // fix this using the state hook
 
   const updateForm = (inputName, inputValue) => {
-    // 🔥 STEP 8 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
-    //  It takes in the name of an input and its value, and updates `formValues`
+    setFormValues({ ...formValues, [inputName]: inputValue});
   }
 
   const submitForm = () => {
-    // 🔥 STEP 9 - IMPLEMENT a submit function which will be used inside the form's own `onSubmit`
-    //  a) make a new friend object, trimming whitespace from username and email
-    //  b) prevent further action if either username or email or role is empty string after trimming
-    //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
-    //  d) also on success clear the form
+    const newFriend = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    if (!newFriend.username || !newFriend.email || !newFriend.role) {
+      setFormError('Username, email AND role are required!')
+    }
+
+    axios.post('fakeapi.com', newFriend)
+    .then(res => {
+      console.log(res)
+      setFriends([res.data, ...friends]);
+      setFormValues(initialFormValues);
+    })
+      .catch(err => {
+      console.error(err);    
+    })
+
   }
 
   useEffect(() => {
@@ -38,12 +52,14 @@ export default function App() {
   return (
     <div className='container'>
       <h1>Form App</h1>
-
+      {formError && <h2 className="error">{formError}</h2>}
       <FriendForm
         // 🔥 STEP 2 - The form component needs its props.
         //  Check implementation of FriendForm
         //  to see what props it expects.
-        values={{}}
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
       />
 
       {
